d3b561374e5e6a2febd50161e826ffbb424b8617
lnqbat <b.lonqueu98@gmail.com>
2025-05-06 13:43:35 +0200

    fix eror checker def main in code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
 
-argc = len(argv) - 1
 
-if argc == 0:
-    print("0 arguments.")
-else:
-    print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+def main():
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
