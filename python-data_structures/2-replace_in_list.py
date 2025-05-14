cc1dbcc0c3caa5938bdaa7f0728c0b189e10cc98
lnqbat <b.lonqueu98@gmail.com>
2025-05-14 11:04:57 +0200

    fix error replace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        new_list = my_list.copy()
-        new_list[idx] = element
-        return new_list
-    return my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
