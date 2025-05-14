0a16f820a1672e4a8b1959f573f887f6346cc3c1
lnqbat <b.lonqueu98@gmail.com>
2025-05-14 11:21:03 +0200

    add condition len

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
 
-    if (a_dictionary is None):
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     result = max(a_dictionary, key=a_dictionary.get)
     return result
