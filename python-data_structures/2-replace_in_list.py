cf81aee194ad6e2cd1271f0312938ca7d8d3debc
lnqbat <b.lonqueu98@gmail.com>
2025-05-14 11:03:55 +0200

    fix condition eror

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-
-    my_list[idx] = element
-    return my_list
+    if 0 <= idx < len(my_list):
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
+    return my_list.copy()
