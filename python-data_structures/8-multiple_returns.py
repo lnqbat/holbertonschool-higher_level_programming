1eb1f0b7b31377f7706743a115d400a74f497611
lnqbat <b.lonqueu98@gmail.com>
2025-05-14 11:12:56 +0200

    fix  return none missed

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
     length = len(sentence)
     if length == 0:
-        result = None
-        return result
+        return (0, None)
     else:
-        res = (length, sentence[0:1])
-        return res
+        return (length, sentence[0])
