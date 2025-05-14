16aa85006fd6a858aa7de06ba9235d0c17d5a57c
lnqbat <b.lonqueu98@gmail.com>
2025-05-14 12:51:16 +0200

    fix code to return 0 allways

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
+        div = 0
         try:
             div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
