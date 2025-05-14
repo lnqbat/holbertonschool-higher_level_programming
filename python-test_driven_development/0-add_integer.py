058c6a1d7dd6990374cb30b26c3c48e91228cb3a
lnqbat <b.lonqueu98@gmail.com>
2025-05-14 14:26:17 +0200

    add coment in files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+
+"""
+function that adds 2 integers.
+"""
+
+
 def add_integer(a, b=98):
+    """
+    that add integer
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
