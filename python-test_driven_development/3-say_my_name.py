1018e3685528b60b02c0fc5131f1195943840b39
lnqbat <b.lonqueu98@gmail.com>
2025-05-15 14:01:20 +0200

    Add comentary on code

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+
+"""
+Print say my name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+
+    """
+    That function say my name
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
