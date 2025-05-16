5e08a52e76298511785e515286b222faf8f6d602
lnqbat <b.lonqueu98@gmail.com>
2025-05-16 10:24:28 +0200

    that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Text indentation
+"""
+
+
+def text_indentation(text):
+    """
+    Prints text with two newlines
+    """
+
+    i = 0
+    while i < len(text):
+        print("{}".format(text[i]), end="")
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
