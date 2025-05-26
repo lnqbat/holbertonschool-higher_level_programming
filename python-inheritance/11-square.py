ca63be3ff111c6e4b2cb932d1280686ab6358215
lnqbat <b.lonqueu98@gmail.com>
2025-05-26 17:00:41 +0200

    Add function that add __str__ return message

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Class for future geometry operations.
+    """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    That def Rectangle.
+    """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square that inherits
+    """
+    def __init__(self, size):
+        """
+        Init square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Area a square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
