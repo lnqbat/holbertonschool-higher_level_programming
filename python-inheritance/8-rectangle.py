7610d8b1b207ae9a42f4cfb5d3e84b5a965ac9ba
lnqbat <b.lonqueu98@gmail.com>
2025-05-26 15:06:58 +0200

    check before enter value

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(BaseGeometry):
         """
         Init Rectangle width and height.
         """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
