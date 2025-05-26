11ef49ac13fe60e699a10cce452b6ee82f2540c4
lnqbat <b.lonqueu98@gmail.com>
2025-05-26 15:13:01 +0200

    Test alternating width and height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -33,6 +33,6 @@ class Rectangle(BaseGeometry):
         Init Rectangle width and height.
         """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
