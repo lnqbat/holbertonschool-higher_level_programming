c232f55b096df3a4891ac0bc69d268e3e1242405
lnqbat <b.lonqueu98@gmail.com>
2025-05-26 11:46:42 +0200

    add function area Raises an Exception to indicate that the method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Class for future geometry operations.
+    """
+    def area(self):
+        """
+        The area Raises an Exception to indicate that the method.
+        """
+        raise Exception("area() is not implemented")
