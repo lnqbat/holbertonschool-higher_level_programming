1870ed6676a9257467fad829cc1ba96b2f56c744
lnqbat <b.lonqueu98@gmail.com>
2025-06-11 11:08:17 +0200

    add simple HTTP server implementation with GET endpoints

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,53 @@
+import http.server
+import socketserver
+import json
+
+PORT = 8000
+
+
+class MyRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.path
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            message = "Hello, this is a simple API!"
+            self.wfile.write(message.encode("utf-8"))
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.send_header()
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+                }
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.send_header()
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content_Type", "application/json")
+            self.send_header()
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                }
+            self.wfile.write(json.dumps(info).encode("utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.send_header()
+            self.wfile.write(b"Endpoint not found")
+
+with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
+    print(f"Serveur lancé sur le port {PORT}")
+    httpd.serve_forever()
