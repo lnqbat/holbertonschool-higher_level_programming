27d2538394dbf5774ce8ae90dd2d7b2607d77349
lnqbat <b.lonqueu98@gmail.com>
2025-06-12 23:03:56 +0200

    feat: add basic authentication and JWT protection to Flask API

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+from flask_httpauth import HTTPBasicAuth
+from flask import Flask
+from flask import jsonify
+from flask import request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import (
+    JWTManager, create_access_token, jwt_required,
+    get_jwt_identity
+)
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "#BaptisteLeBGdu31!"
+
+auth = HTTPBasicAuth()
+jwt = JWTManager(app)
+
+users = {
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+    },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+    }
+}
+@auth.verify_password
+def verify_password(username, password):
+    user = users.get(username)
+    if user and check_password_hash(user["password"], password):
+        return username
+    return None
+
+
+@app.route("/basic-protected", methods=["GET"])
+@auth.login_required
+def basic_protected():
+    return "Basic Auth: Access Granted"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = users.get(username)
+    if not user or not check_password_hash(user["password"], password):
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity={"username": username, "role": user["role"]})
+    return jsonify(access_token=access_token)
+
+@app.route("/jwt-protected", methods=["GET"])
+@jwt_required()
+def protected():
+    return "JWT Auth: Access Granted"
+
+@app.route("/admin-only", methods=["GET"])
+@jwt_required()
+def admin():
+    current_user = get_jwt_identity()
+    if current_user.get("role") != "admin":
+        return jsonify({"error": "Admin access required"}), 403
+    return "Admin Access: Granted"
