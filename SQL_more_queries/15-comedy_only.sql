01cb42f71dc704f8cf67260eea857a854cb18104
lnqbat <b.lonqueu98@gmail.com>
2025-06-24 16:12:31 +0200

    feat: add SQL script to list all Comedy TV shows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Lists all Comedy
+SELECT tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+  AND tv_show_genres.genre_id = tv_genres.id
+  AND tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
