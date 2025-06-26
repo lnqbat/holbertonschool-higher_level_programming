93cf9a1b65423ad2f6888ac0e62559cc3c7ba5ca
lnqbat <b.lonqueu98@gmail.com>
2025-06-26 15:11:25 +0200

    feat: add script to list all State objects from the database

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Liste tous les objets State de la base hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(State).order_by(State.id).all()
+
+    for state in results:
+        print("{}: {}".format(state.id, state.name))
