8f3bc565fab86b33144c2f7f0787c2b52cb9f9ce
lnqbat <b.lonqueu98@gmail.com>
2025-06-27 14:45:17 +0200

    style: correct script header comment formatting

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-script 14-model_city_fetch_by_state.py
-that prints all City objects from the database hbtn_0e_14_usa
+Script that prints all City objects from the database hbtn_0e_14_usa
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
