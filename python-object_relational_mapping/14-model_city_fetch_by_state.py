06e05ead6e72bb050a7aaa999af94a9636d0d243
lnqbat <b.lonqueu98@gmail.com>
2025-06-27 14:35:06 +0200

    fix: import Base from model_state for proper ORM setup

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,7 +6,7 @@ that prints all City objects from the database hbtn_0e_14_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
