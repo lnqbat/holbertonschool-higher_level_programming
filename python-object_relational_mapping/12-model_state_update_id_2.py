c951841236b398195b2f8b79b1f7a61f8d3c4fa6
lnqbat <b.lonqueu98@gmail.com>
2025-06-27 10:23:54 +0200

    feat: update State object name from "Louisiana" to "New Mexico" in the database

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that adds the State object “Louisiana” to the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
