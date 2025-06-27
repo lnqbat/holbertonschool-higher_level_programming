72466f0ab9ee86d6a6a95a6fbb7dc00ff8728270
lnqbat <b.lonqueu98@gmail.com>
2025-06-27 14:28:39 +0200

    fix: ensure case-sensitive state name matching in query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = ("SELECT * FROM states WHERE name = '{}' "
+    query = ("SELECT * FROM states WHERE BINARY name = '{}' "
         "ORDER BY id ASC".format(state_name)
     )
     cur.execute(query)
