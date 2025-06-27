ef802a33dbf6369c69b04f6777d0df22e9e67100
lnqbat <b.lonqueu98@gmail.com>
2025-06-27 13:49:56 +0200

    feat: add script to fetch and print all City objects from the database

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script 14-model_city_fetch_by_state.py 
+that prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(State.name, City.id, City.name)\
+                     .join(City, City.state_id == State.id)\
+                     .order_by(City.id)\
+                     .all()
+
+    for state_name, city_id, city_name in results:
+        print("{}: ({}) {}".format(state_name, city_id, city_name))
