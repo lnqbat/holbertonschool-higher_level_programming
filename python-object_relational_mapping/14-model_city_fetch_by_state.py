1b3d95fd26711ffdd016949af88415e41a4842e8
lnqbat <b.lonqueu98@gmail.com>
2025-06-27 13:52:21 +0200

    style: fix formatting of docstring in 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script 14-model_city_fetch_by_state.py 
+script 14-model_city_fetch_by_state.py
 that prints all City objects from the database hbtn_0e_14_usa
 """
 import sys
