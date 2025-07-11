8eb904d2440fd379abcc01b46437b2406fb6b276
lnqbat <b.lonqueu98@gmail.com>
2025-07-11 11:00:16 +0200

    feat: add factorial calculation script

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+function fact (n) {
+    let res = 1;
+    for (let i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
+}
+console.log(fact(argv[2]));
