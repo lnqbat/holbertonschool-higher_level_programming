bba1d44a6e860e6f6a35e0900e0a88f2fef8c880
lnqbat <b.lonqueu98@gmail.com>
2025-07-11 10:36:41 +0200

    feat: add script to print 'C is fun' a specified number of times

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const x = parseInt(argv[2]);
+let i;
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
